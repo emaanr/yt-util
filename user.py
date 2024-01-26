bb63a6e6c57ef9a03efe9ee7eb59d5daf6325141
Emaan Rana <87550510+emaanr@users.noreply.github.com>
2024-01-26 06:59:10 -0500

    User themes for ttkbootstrap

---
+++ user.py
@@ -0,0 +1,44 @@
+USER_THEMES = {
+    "yt-dark": {
+        "type": "dark",
+        "colors": {
+            "primary": "#ff0000",
+            "secondary": "#272727",
+            "success": "#00d4b5",
+            "info": "#3ea6ff",
+            "warning": "#ff76da",
+            "danger": "#ff0000",
+            "light": "#ffffff",
+            "dark": "#272727",
+            "bg": "#0f0f0f",
+            "fg": "#ffffff",
+            "selectbg": "#4d4d4d",
+            "selectfg": "#ffffff",
+            "border": "#373737",
+            "inputfg": "#ffffff",
+            "inputbg": "#272727",
+            "active": "#4d4d4d"
+        }
+    },
+    "yt-light": {
+        "type": "light",
+        "colors": {
+            "primary": "#ff0000",
+            "secondary": "#e5e5e5",
+            "success": "#00d4b5",
+            "info": "#3ea6ff",
+            "warning": "#ff67da",
+            "danger": "#ff0000",
+            "light": "#e5e5e5",
+            "dark": "#272727",
+            "bg": "#ffffff",
+            "fg": "#0f0f0f",
+            "selectbg": "#e5e5e5",
+            "selectfg": "#0f0f0f",
+            "border": "#ededed",
+            "inputfg": "#0f0f0f",
+            "inputbg": "#f8f8f8",
+            "active": "#e5e5e5"
+        }
+    }
+}
